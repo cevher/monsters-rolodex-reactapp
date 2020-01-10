46af99e9dbdf57b07ac1a8d4d4efac8174d415b5
LAPTOP-7C5H1RJ1\Cevher <efeozden@gmail.com>
2020-01-10 11:35:49 +0300

    Updates

---
+++ precache-manifest.65c32e85e52e986561c72e80a8af102d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4931a62887802a4ed5aa7bda9030362",
+    "url": "/monsters-rolodex-reactapp/index.html"
+  },
+  {
+    "revision": "9d5ce48f5e4a3cf993bb",
+    "url": "/monsters-rolodex-reactapp/static/css/main.146a2261.chunk.css"
+  },
+  {
+    "revision": "316fe34f780067e66c1e",
+    "url": "/monsters-rolodex-reactapp/static/js/2.662d7688.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex-reactapp/static/js/2.662d7688.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d5ce48f5e4a3cf993bb",
+    "url": "/monsters-rolodex-reactapp/static/js/main.063b7972.chunk.js"
+  },
+  {
+    "revision": "b5b4d456cd73b60facd0",
+    "url": "/monsters-rolodex-reactapp/static/js/runtime-main.03cb5c04.js"
+  }
+]);
